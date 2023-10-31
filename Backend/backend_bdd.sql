b4602a657a8b10cd5d41e23c5c08dc4c4c02c442
root <root@Victus-R>
2023-10-31 16:58:51 +0100

    mise à jour BDD

--- Backend/backend_bdd.sql
+++ Backend/backend_bdd.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.1.0, for macos14.0 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.35, for Linux (x86_64)
 --
 -- Host: localhost    Database: consonea
 -- ------------------------------------------------------
--- Server version	8.1.0
+-- Server version	8.0.35-0ubuntu0.22.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -111,4 +111,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-10-31 14:37:21
+-- Dump completed on 2023-10-31 16:58:22
