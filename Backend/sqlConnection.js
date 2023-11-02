b58dd278907d069600baf20471463e87eb3b6f26
root <root@Victus-R>
2023-11-02 15:34:41 +0100

    test changement de mdp accès bdd sqlConnction.js

--- Backend/sqlConnection.js
+++ Backend/sqlConnection.js
@@ -4,12 +4,13 @@ const mysql = require("mysql");
 let db_con = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: ''
+    password: '',
+    database : 'consonea'
 });
 
 db_con.connect((err) => {
     if (err) {
-        console.log("Database Connection Failed !!!", err);
+       return console.log("Database Connection Failed !!!", err);
     } else {
         console.log("connected to Database");
     }
