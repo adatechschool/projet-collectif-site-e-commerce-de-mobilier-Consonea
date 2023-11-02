d021ec8598616aff9be78880d6c6c3e00cfa3e93
FranceHuon <133089058+FranceHuon@users.noreply.github.com>
2023-11-02 16:03:56 +0100

    homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,36 @@
-export default function Home() {
-    return (
-        <h1>homepage</h1>
-    );
+import React from "react";
+import Article from "../components/Article" ;
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+
+function Home() {
+  return (
+    <div className="Article">
+      <h1>Donnez </h1>
+
+<Container>
+    <Row>
+        <Col><Article /></Col>
+        <Col><Article /></Col>
+        <Col><Article /></Col>
+    </Row>
+    <Row>
+        <Col><Article /></Col>
+        <Col><Article /></Col>
+        <Col><Article /></Col>
+    </Row>
+    <Row>
+        <Col><Article /></Col>
+        <Col><Article /></Col>
+        <Col><Article /></Col>
+    </Row>
+</Container>
+
+      
+    </div>
+  );
 }
+
+export default Home
