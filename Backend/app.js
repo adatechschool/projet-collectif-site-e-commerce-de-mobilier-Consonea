d2536ed1cc641b654d3d81d7893fddce1a994313
PaulaRondao <134066088+PaulaRondao@users.noreply.github.com>
2023-11-03 17:00:49 +0100

    Update - app.js + sqlConnection
    Mise à jour d'une interface pour communiquer avec la base de données. Création d'une classe d'objet.

--- Backend/app.js
+++ Backend/app.js
@@ -1,39 +1,53 @@
-const express = require('express'); // importe le module 
+const express = require('express');  
 const app = express(); // j'initialise express
+const port = 3000; 
+
+const dataService = require('./sqlConnection');
 
 
-// const port = 3000; pour la création du Port, à utiliser avec la seconde possibilité plus bas
+// --------- Pour tester si le serveur Express fonctionne ------------
+app.use((req, res) => {
+    res.json({ message: 'nous avons reçu votre requête' });
+});
 
 
 
+// --------------- Create -----------
+app.post('/insert', (request, response) => {
+    // Récupère le nom depuis le corps de la requête
+    const { name } = request.body;
+    // Obtient une instance de la base de données
+    const db = dataService.getDatabaseObjetInstance();
+
+    // Insère le nouveau nom dans la base de données
+    const result = db.insertNewName(name);
+
+    // Gère la réponse une fois l'opération terminée
+    result
+        .then(data => response.json({ data: data }))
+        .catch(err => console.log(err));
+});
+
+
+
+// --------------- READ ------------
 // Permet d'écouter les requêtes envoyées par le client. 
-// ('/') = la route racine, (call back avec deux paramètre : requête et réponse) 
+// ('/') = chemin relatif à la racine du site, (call back avec deux paramètre : requête et réponse) 
 // function fléchée qui sera exécuté lorsque le client va envoyer une requête à cette route 
-app.get('/index', (req, res) => {
+app.get('/', (req, res) => {
     res.send("<h1>Salut le monde<h1>");
     // res.status(200).sendFile('index.html', {root: __dirname}); // pour envoyé un ficher entier (senfile) suite à la réquête du client 
 });
 
 
 
-// const database = require('../Backend/sqlConnection');
-
-
-// Seconde possibilité à utiliser avec la function port (créé plus haut)
-// app.listen(port, () => {
-//     console.log(`Application exemple à l'écoute sur le port ${port}!`);
-//   });
-
-
-
-app.listen(3000, () => {
-    console.log(`Server is up and running on 3000 ...`);
+// ---------- LISTEN ----------
+app.listen(port, () => {
+    console.log(`Application exemple à l'écoute sur le port ${port}!`);
 });
 // pour écouter le serveur, on utilise la méthode listen qui prend en paramètre le 'PORT', le 'domaine qui
 // est localhost, pas nécessaire de le nommer puisqu'il il est mis par défaut, puis un callback () - fonction fléchée qui sera 
 // exécuté quand le serveur sera prêt à recevoir une requête
 
 
-app.use((req, res) => {
-    res.json({ message: 'nous avons reçu votre requête' });
-});
+
--- Backend/sqlConnection.js
+++ Backend/sqlConnection.js
@@ -1,19 +1,51 @@
+// cette page sert d'interface pour interagir avec la base de données
 
 const mysql = require("mysql");
 
-let db_con = mysql.createConnection({
+
+const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: '',
-    database : 'consonea'
+    database: 'consonea',
+    port: process.env.PORT || 3000 //process.env.PORT : c'est pour lancer un port par défaut si 3000 n'est pas dispo
 });
 
-db_con.connect((err) => {
+
+connection.connect((err) => {
     if (err) {
-       return console.log("Database Connection Failed !!!", err);
+        return console.log("Database Connection Failed !!!", err);
     } else {
         console.log("connected to Database");
     }
 });
 
-module.exports = db_con;
+
+// 
+class DatabaseObjet {
+    static getDataObjetInstance() {
+        return instance ? instance : new DatabaseObjet();
+    } //la syntaxe ? : appelé opérateur ternaire : expression à tester ? valeur si vrai : valeur si faux. 
+
+    async getAllData() {
+        try {
+            const response = await new Promise((resolve, reject) => {
+                const query = "SELECT * FROM articles;";
+
+                connection.query(query, (err, results) => {
+                    if (err) reject(new Error(err.message));
+                    resolve(results);
+                })
+            });
+            console.log(response);
+            return response;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+
+
+
+module.exports = connection;
