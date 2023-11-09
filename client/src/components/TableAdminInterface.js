092c2cd9cff62182a712e81b8b23a2380fbf4729
NathanPesneau <npesneau@MacBook-Pro-de-Pesneau.local>
2023-11-09 14:33:05 +0100

    ajout boutons supprimer/valider DONE lier a delete & patch WIP

--- client/src/components/TableAdminInterface.js
+++ client/src/components/TableAdminInterface.js
@@ -1,6 +1,21 @@
 import Table from 'react-bootstrap/Table';
 import React, { useState, useEffect } from 'react';
 
+function showItem(object_id) {
+  var raw = "";
+
+var requestOptions = {
+  method: 'PATCH',
+  body: raw,
+  redirect: 'follow'
+};
+
+fetch("localhost:5500/articles/1", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+}
+
 
 function TableAdmin() {
   const [articles, setArticles] = useState([]);
@@ -9,6 +24,7 @@ function TableAdmin() {
     console.log('got through the function')
 
     useEffect(() => {
+      // on récupère l'ensemble des articles de la db
       fetch("http://localhost:5500/articles")
         .then(response => response.json())
         .then(json => setArticles(json))
@@ -31,16 +47,23 @@ function TableAdmin() {
         </thead>
         <tbody>
         {articles.map(article => {
-            // return <pre>{JSON.stringify(articles['type'])}</pre>
-        
+          if (article.status === 0)
           return (<tr>
           <td>{article.user_id}</td>
           <td>{article.name}</td>
           <td>{article.type}</td>
-          <td>{article.prix}</td>
-          <td>{article.status}</td>
-          <td>Mettre en vente</td>
+          <td>{article.price + ' €'}</td>
+          <td>A valider</td>
+          <td><button onClick={showItem} class="mt-4 btn btn-success">valider</button></td>
           </tr>)
+          else return((<tr>
+            <td>{article.user_id}</td>
+            <td>{article.name}</td>
+            <td>{article.type}</td>
+            <td>{article.price + ' €'}</td>
+            <td>En vente</td>
+            <td><button class="mt-4 btn btn-success">supprimer</button></td>
+            </tr>))
           })}
         </tbody>
       </Table>
